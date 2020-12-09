23285f6f10910a394cf7578d2b74aa05d89c6a99
Nikhil Rawat <nikhilrawat850@gmail.com>
2020-12-09 22:24:53 +0530

    feat: add react project

---
+++ src/components/TextField/style.js
@@ -0,0 +1,12 @@
+const stylesheet = {
+  input: {
+    padding: '10px',
+    boxSizing: 'border-box',
+    width: '100%',
+  },
+  errorinput: {
+    border: '1px solid red',
+  },
+};
+
+export default stylesheet;
