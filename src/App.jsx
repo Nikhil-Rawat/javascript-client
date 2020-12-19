c64d95349e56bb3cb0bd0dcd708185cf7a8377e7
Nikhil Rawat <nikhilrawat850@gmail.com>
2020-12-19 10:53:15 +0530

    Feature:40835 [Material UI- Styling, Dialog, Form Validation]

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 // import { TextFieldDemo } from './pages';
 import { theme } from './theme';
 // import { InputDemo } from './pages';
-import { ChildrenDemo } from './pages';
+// import { ChildrenDemo } from './pages';
+import { Trainee } from './pages/Trainee';
 
 function App() {
   const classes = theme();
   return (
     <div className={classes.root}>
-      <ChildrenDemo />
+      <Trainee />
     </div>
   );
 }
---
+++ src/pages/Trainee/components/AddDialog/AddDailog.jsx
@@ -0,0 +1,183 @@
+/* eslint-disable no-console */
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { func, bool } from 'prop-types';
+import { InputAdornment } from '@material-ui/core';
+import PersonIcon from '@material-ui/icons/Person';
+import EmailIcon from '@material-ui/icons/Email';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+import * as yup from 'yup';
+
+const FormDialog = (props) => {
+  const { onClose, open, onSubmit } = props;
+
+  const [state, setstate] = useState({
+    Name: '', Email: '', Password: '', Confirm: '',
+  });
+  const [blur, setblur] = useState({
+    Name: false, Email: false, Password: false, Confirm: false,
+  });
+  const schema = yup.object().shape({
+    Name: yup.string().required().min(3),
+    Email: yup.string().email().required(),
+    Password: yup.string().required().min(8).matches(/^(?=.{10,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$/, 'Password must be alphanumeric'),
+    Confirm: yup.string().oneOf([yup.ref('Password'), null], 'Password must match'),
+  });
+
+  const handleNameChange = (event) => {
+    setstate({ ...state, Name: event.target.value });
+  };
+
+  const handlePasswordChange = (event) => {
+    setstate({ ...state, Password: event.target.value });
+  };
+
+  const handleConfirmChange = (event) => {
+    setstate({ ...state, Confirm: event.target.value });
+  };
+
+  const handleEmailChange = (event) => {
+    setstate({ ...state, Email: event.target.value });
+  };
+
+  const hasError = () => {
+    try {
+      return !schema.validateSync(state);
+    } catch (err) {
+      return true;
+    }
+  };
+
+  const handleBlur = (field) => {
+    setblur({ ...blur, [field]: true });
+  };
+
+  const isTouched = () => (blur.Name || blur.Password || blur.Confirm || blur.Email);
+
+  const getError = (field) => {
+    if (blur[field] && hasError()) {
+      try {
+        schema.validateSyncAt(field, state);
+      } catch (err) {
+        return err.message;
+      }
+    }
+    return null;
+  };
+  return (
+    <div>
+      <Dialog open={open} onClose={onClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Add Trainee</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Enter your trainee details
+          </DialogContentText>
+          <TextField
+            required
+            margin="dense"
+            id="name"
+            label="Name"
+            type="name"
+            variant="outlined"
+            error={getError('Name')}
+            helperText={getError('Name')}
+            onChange={handleNameChange}
+            onBlur={() => handleBlur('Name')}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <PersonIcon />
+                </InputAdornment>
+              ),
+            }}
+            fullWidth
+          />
+          <TextField
+            margin="dense"
+            id="email"
+            label="Email"
+            type="email"
+            variant="outlined"
+            error={getError('Email')}
+            helperText={getError('Email')}
+            onChange={handleEmailChange}
+            onBlur={() => handleBlur('Email')}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <EmailIcon />
+                </InputAdornment>
+              ),
+            }}
+            fullWidth
+          />
+          <form>
+            <TextField
+              margin="dense"
+              id="password"
+              label="Password"
+              type="new-password"
+              variant="outlined"
+              error={getError('Password')}
+              helperText={getError('Password')}
+              onChange={handlePasswordChange}
+              onBlur={() => handleBlur('Password')}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <VisibilityOffIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              margin="dense"
+              id="confirm"
+              label="Confirm"
+              type="new-password"
+              variant="outlined"
+              error={getError('Confirm')}
+              helperText={getError('Confirm')}
+              onChange={handleConfirmChange}
+              onBlur={() => handleBlur('Confirm')}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <VisibilityOffIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={() => { onSubmit(state); }} color="primary" disabled={hasError() && isTouched()}>
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+FormDialog.defaultProps = {
+  open: false,
+  onSubmit: null,
+};
+
+FormDialog.propTypes = {
+  open: bool,
+  onClose: func.isRequired,
+  onSubmit: func,
+};
+
+export default FormDialog;
