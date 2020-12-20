7a0c443c1c0660bf05085c8834af44d4c2d83c04
Nikhil Rawat <nikhilrawat850@gmail.com>
2020-12-20 20:55:26 +0530

    Add Cssbaseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 // import { TextFieldDemo } from './pages';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { theme } from './theme';
 // import { InputDemo } from './pages';
 // import { ChildrenDemo } from './pages';
@@ -10,6 +11,7 @@ function App() {
   const classes = theme();
   return (
     <div className={classes.root}>
+      <CssBaseline />
       <FormDialog />
     </div>
   );
